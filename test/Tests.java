a740f4cf92c2ac790c49c6ae6816fb8700e2e28f
KuzinaMari <Novamaria98@mail.ru>
2017-04-12 13:28:23 +0300

    Update Tests.java

--- test/Tests.java
+++ test/Tests.java
@@ -9,22 +9,24 @@ import org.junit.jupiter.api.Tag;
 
 
 class tests {
-    public Double_number m1 = new Double_number("12.419");
-    public Double_number m2 = new Double_number("78767.0");
-    public Double_number m3 = new Double_number("8753.0");
+    private Double_number m1 = new Double_number("12.419");
+    private Double_number m2 = new Double_number("12.0");
+    private Double_number m3 = new Double_number("24.419");
+    private Double_number m4 = new Double_number("-0.419");
+    private Double_number m5 = new Double_number("149.028");
 
 
     @Test
     @Tag("toFloat")
-    void toFloat()  {
-        assertEquals((new Double_number("12")).toFloat(), 12);
+    void toFloat() {
+        assertEquals(m1.toFloat(), 12, 419);
     }
 
 
     @Test
     @Tag("toLong")
     void toLong() {
-        assertEquals((new  Double_number("12")).toLong(), 12);
+        assertEquals(m2.toLong(), 78767);
     }
 
     @Test
@@ -34,19 +36,36 @@ class tests {
             new Double_number("156.76876").toIntE();
             assertEquals("IllegalArgumentException", "---");
         } catch (IllegalArgumentException e) {
-            assertEquals( 12, 12);
+            assertEquals(12, 12);
         }
     }
 
     @Test
     @Tag("toDouble")
     void toDouble() {
-        assertEquals(12.42, (new Double_number("12.42")).toDouble());
+        assertEquals(12.419, m1.toDouble());
     }
+
     @Test
     @Tag("curcle")
     void curcle() {
-//System.out.print(m1+"\n"+ m2+"\n" + m3+"\n"+m4);
         assertEquals(12.42, m1.curcle(2).toDouble());
+    }
+
+    @Test
+    @Tag("Sum")
+    void sum() {
+        assertEquals(m3, m1.sum(m2));
+    }
+
+    @Test
+    @Tag("Minus")
+    void minus() {
+        assertEquals(m4, m2.minus(m1));
+    }
+    @Test
+    @Tag("Mult")
+    void multi() {
+        assertEquals(m5, m2.multi(m1));
     }
 }
